b5476ea964f0767c55daad78610dfd3b157d68f0
jarkor5 <67030533+jarkor5@users.noreply.github.com>
2023-07-13 01:31:23 +0000

    first commit

--- src/js/app.js
+++ src/js/app.js
@@ -28,19 +28,55 @@ function render(variables = {}) {
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  let name = ` ${variables.name} `;
+  let country = ` ${variables.country} `;
+  let lastname = ` ${variables.lastname} `;
+  let role = ` ${variables.role} `;
+  let city = ` ${variables.city} `;
+  let twitter = `${variables.twitter}`;
+  let linkedin = `${variables.linkedin}`;
+  let github = `${variables.github}`;
+  let instagram = `${variables.instagram}`;
+  if (variables.name == null) {
+    name = `Jacobo`;
+  }
+  if (variables.lastname == null) {
+    lastname = `Ospina `;
+  }
+  if (variables.role == null) {
+    role = `Web developer `;
+  }
+  if (variables.city == null) {
+    city = `Miami`;
+  }
+  if (variables.country == null) {
+    country = `USA`;
+  }
 
-  // reset the website body with the new html output
+  if (variables.twitter == null) {
+    twitter = `Jarkor5`;
+  }
+  if (variables.linkedin == null) {
+    linkedin = `Jarkor52`;
+  }
+  if (variables.github == null) {
+    github = `Jarkor52`;
+  }
+
+  if (variables.instagram == null) {
+    instagram = `Jacob0_el_Rakfar`;
+  } // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1> ${name} ${lastname} </h1>
+          <h2>${role}</h2>
+          <h3>${city}, ${country}</h3>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter">${twitter}</i></a></li>
+            <li><a href="https://github.com/4geeksacademy">${github}<i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/4geeksacademy">${linkedin}<i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/4geeksacademy">${instagram}<i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
